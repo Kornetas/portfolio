f90399f9549010d0f5f632b9a303c76e38985e96
Arek <arek93r@gmail.com>
2025-05-25 17:41:44 +0200

    build home section with neon style and animated resume button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "neon-green": "#39FF14",
+      },
+      boxShadow: {
+        "neon-glow": "0 0 16px 2px #39FF14, 0 0 40px 4px #39FF14",
+      },
+    },
   },
   plugins: [],
 };
