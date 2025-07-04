ebeaddf993610f9ea6fedf1374450b00b93d4216
Arek <arek93r@gmail.com>
2025-07-04 20:00:42 +0200

    Adjust home section layout for different device sizes

--- src/components/HeroAvatar.jsx
+++ src/components/HeroAvatar.jsx
@@ -11,7 +11,7 @@ export default function HeroAvatar({ className = "", variant = "desktop" }) {
         alt="Arkadiusz Rak – Full Stack Web Developer"
         className={
           variant === "desktop"
-            ? "object-contain md:h-[80vh] md:max-h-[1050px] w-auto opacity-90 z-10 select-none pointer-events-none hero-avatar-filter"
+            ? "object-contain md:h-[80vh] md:max-h-[1050px] w-auto opacity-90 z-10 md:mt-36 select-none pointer-events-none hero-avatar-filter"
             : "object-contain h-[38vh] xs:h-[45vh] sm:h-[52vh] max-h-[350px] w-auto opacity-90 select-none pointer-events-none hero-avatar-mobile-filter"
         }
         draggable="false"
