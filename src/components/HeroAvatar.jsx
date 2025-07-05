e24d0c071586dbd1054eb5c5ce10c164d486a4f0
Arek <arek93r@gmail.com>
2025-07-05 19:09:29 +0200

    Fix mobile avatar scaling issue in Chrome on iOS

--- src/components/HeroAvatar.jsx
+++ src/components/HeroAvatar.jsx
@@ -12,7 +12,7 @@ export default function HeroAvatar({ className = "", variant = "desktop" }) {
         className={
           variant === "desktop"
             ? "object-contain md:h-[80vh] md:max-h-[1050px] w-auto opacity-90 z-10 md:mt-36 select-none pointer-events-none hero-avatar-filter"
-            : "object-contain h-[38vh] xs:h-[45vh] sm:h-[52vh] max-h-[350px] w-auto opacity-90 select-none pointer-events-none hero-avatar-mobile-filter"
+            : "object-contain max-h-[350px] w-full h-auto opacity-90 select-none pointer-events-none hero-avatar-mobile-filter"
         }
         draggable="false"
       />
